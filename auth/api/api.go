039ba650cb2cbb6962ebb53c0f3fecdcdce14ff3
Rey David <rdominguez@tecnologer.net>
2024-03-13 11:24:16 -0700

    created auth microsevice

---
+++ auth/api/api.go
@@ -0,0 +1,103 @@
+package api
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+
+	"github.com/sirupsen/logrus"
+
+	"deuna.com/payment/auth/models"
+	"github.com/pkg/errors"
+)
+
+type Auth struct {
+	host string
+}
+
+func NewAuth(host string) *Auth {
+	return &Auth{
+		host: host,
+	}
+}
+
+func (a *Auth) Login(username string, password string) (string, error) {
+	credentials := &models.Credential{
+		Username: username,
+		Password: password,
+	}
+
+	credentialsJson, err := json.Marshal(credentials)
+	if err != nil {
+		return "", errors.Wrap(err, "marshalling credentials")
+	}
+
+	res, err := http.Post(a.host+"/login", "application/json", bytes.NewReader(credentialsJson))
+	if err != nil {
+		return "", errors.Wrap(err, "request login")
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return "", errors.Errorf("invalid status code: %d", res.StatusCode)
+	}
+
+	var token map[string]string
+
+	err = json.NewDecoder(res.Body).Decode(&token)
+	if err != nil {
+		return "", errors.Wrap(err, "decoding token")
+	}
+
+	return token["token"], nil
+}
+
+func (a *Auth) IsAuthorized(token string) (bool, error) {
+	req, err := http.NewRequest("GET", a.host+"/validate", nil)
+	if err != nil {
+		return false, errors.Wrap(err, "creating request")
+	}
+
+	logrus.Debug("token: ", token)
+
+	req.Header.Set("Authorization", token)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return false, errors.Wrap(err, "request validate")
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return false, nil
+	}
+
+	return true, nil
+}
+
+func (a *Auth) GetClaim(token string) (*models.Claim, error) {
+	req, err := http.NewRequest("GET", a.host+"/validate", nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "auth_api.get_claim: creating request")
+	}
+
+	logrus.Debug("token: ", token)
+
+	req.Header.Set("Authorization", token)
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, errors.Wrap(err, "auth_api.get_claim: request validate")
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, nil
+	}
+
+	var claim *models.Claim
+
+	err = json.NewDecoder(res.Body).Decode(&claim)
+	if err != nil {
+		return nil, errors.Wrap(err, "auth_api.get_claim: decoding claim")
+	}
+
+	return claim, nil
+}
---
+++ auth/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/pkg/errors"
+
+	"github.com/sirupsen/logrus"
+
+	"deuna.com/payment/auth/models"
+)
+
+var (
+	port = flag.Int("port", 8080, "Port to run the server on")
+
+	jwtSecret = "$3cr3t#"
+)
+
+func main() {
+	flag.Parse()
+
+	logrus.SetLevel(logrus.DebugLevel)
+
+	if secret := os.Getenv("JWT_SECRET"); secret != "" {
+		jwtSecret = secret
+	}
+
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/validate", validate)
+
+	host := fmt.Sprintf(":%d", *port)
+
+	logrus.Info("Server running on ", host)
+	logrus.Fatal(http.ListenAndServe(host, nil))
+}
+
+// signToken handler to authenticate users and generate JWT
+func login(w http.ResponseWriter, r *http.Request) {
+	var creds models.Credential
+
+	logrus.Debug("signToken request for user: ", creds.Username)
+
+	err := json.NewDecoder(r.Body).Decode(&creds)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tokenString, err := signToken(&creds)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		logrus.WithError(err).Error("getting token")
+
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err = json.NewEncoder(w).Encode(map[string]string{"token": tokenString})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+
+		logrus.WithError(err).Error("Error encoding token")
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	logrus.Debugf("User %s logged in successfully", creds.Username)
+}
+
+func signToken(credentials *models.Credential) (string, error) {
+	expirationTime := time.Now().Add(5 * time.Minute)
+	claims := &models.Claim{
+		Username: credentials.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(jwtSecret))
+	if err != nil {
+		return "", errors.Wrap(err, "auth_api_login: signing token")
+	}
+
+	return tokenString, nil
+}
+
+func validate(w http.ResponseWriter, r *http.Request) {
+	token := r.Header.Get("Authorization")
+	if token == "" {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	token = strings.TrimPrefix(token, "Bearer ")
+
+	logrus.Debug("validate request for token: ", token)
+
+	claims, err := validateToken(token)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		logrus.WithError(err).Error("auth_api_validate: validating token")
+
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	err = json.NewEncoder(w).Encode(claims)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		logrus.WithError(err).Error("auth_api_validate: encoding claims")
+
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
+
+func validateToken(tokenString string) (*models.Claim, error) {
+	claims := &models.Claim{}
+
+	// Convert your jwtSecret to a byte slice if it's stored as a string
+	secret := []byte(jwtSecret)
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return secret, nil
+	})
+	if err != nil {
+		return nil, errors.Wrap(err, "auth_api_validate_token: parsing token")
+	}
+
+	if !token.Valid {
+		return nil, errors.New("auth_api_validate_token: invalid token")
+	}
+
+	return claims, nil
+}
